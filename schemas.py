dce80f00dffdb17235d38b13ce9b05d521a510e9
datDat <32026170+cuocchie@users.noreply.github.com>
2021-11-23 01:55:34 +0700

    update draft tour schema

--- schemas.py
+++ schemas.py
@@ -1,1 +1,33 @@
-//Pydantic Models
+from pydantic import BaseModel
+from datetime import date, datetime
+
+from sqlalchemy import orm
+
+
+class TourBase(BaseModel):
+    tour_id: str
+    place_id: str
+    tourguide_id: str
+    type_id: str
+    transport: str
+    departure_place: str
+    adult_price: str
+    kid_price: str
+    start_date: date
+    end_date: date
+    seat: int
+    tour_desc: str
+
+    class Config:
+        orm_mode = True
+
+
+class TourID(BaseModel):
+    tour_id: str
+
+
+class Tour(TourBase):
+    pass
+
+    class Config:
+        orm_mode: True
