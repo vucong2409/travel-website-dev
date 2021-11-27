3144197bdbbf84c38db03db7a5d55845abd030e5
LAPTOP-DRU42PCK\Mr John <vucong2409@gmail.com>
2021-11-27 15:45:55 +0700

    Add CORS Middleware

--- main.py
+++ main.py
@@ -7,13 +7,26 @@ from typing import final
 from fastapi import FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from api import login_route, tour_route, login_svc
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create all table
 models.database.Base.metadata.create_all(bind=database.engine)
 
+list_origins = ['localhost:3000',
+                'localhost:5000',
+                'falsesight.asia'
+                ]
+
 app = FastAPI()
 app.include_router(login_route.router)
 app.include_router(tour_route.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=list_origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 db_ins = next(database.get_db())
 
